81cf6fa8e4fc920a3f35ee528d0f108b9690655a
Alexandre Segura <mex.zktk@gmail.com>
2018-02-21 15:07:34 +0100

    Update dependencies (Symfony 3.4 & API Platform 2.2).
    - Fix RestaurantFilter.
    - Fix deprecations.
    - Set SYMFONY_DEPRECATIONS_HELPER to "weak".

--- src/AppBundle/Filter/RestaurantFilter.php
+++ src/AppBundle/Filter/RestaurantFilter.php
@@ -2,16 +2,16 @@
 
 namespace AppBundle\Filter;
 
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use AppBundle\Entity\Restaurant;
+use AppBundle\Entity\RestaurantRepository;
 use Doctrine\ORM\QueryBuilder;
 
-use AppBundle\Entity\RestaurantRepository;
-
-final class RestaurantFilter extends SearchFilter
+final class RestaurantFilter extends AbstractFilter
 {
-    public function apply(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
         $request = $this->requestStack->getCurrentRequest();
         if (null === $request) {
@@ -21,9 +21,35 @@ final class RestaurantFilter extends SearchFilter
         $properties = $this->extractProperties($request, Restaurant::class);
         $distance = isset($properties['distance']) ? $properties['distance'] : 3000;
 
-        if (isset($properties['coordinate'])) {
-            list($latitude, $longitude) = explode(',', $properties['coordinate']);
+        if ($property === 'coordinate') {
+            list($latitude, $longitude) = explode(',', $value);
+            $this->logger->info(sprintf('RestaurantFilter :: %s, %s, %s', $latitude, $longitude, $distance));
             RestaurantRepository::addNearbyQueryClause($queryBuilder, $latitude, $longitude, $distance);
         }
+    }
+
+    public function getDescription(string $resourceClass): array
+    {
+        // if (!$this->properties) {
+        //     return [];
+        // }
+
+        // $description = [];
+        // foreach ($this->properties as $property => $strategy) {
+        //     $description["regexp_$property"] = [
+        //         'property' => $property,
+        //         'type' => 'string',
+        //         'required' => false,
+        //         'swagger' => [
+        //             'description' => 'Filter using a regex. This will appear in the Swagger documentation!',
+        //             'name' => 'Custom name to use in the Swagger documentation',
+        //             'type' => 'Will appear below the name in the Swagger documentation',
+        //         ],
+        //     ];
+        // }
+
+        // return $description;
+
+        return [];
     }
 }
